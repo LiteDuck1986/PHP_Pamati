a548902cce3f06356fa7f18dc722c597895b5008
LiteDuck1986 <emils2007smirnovs@gmail.com>
2026-04-14 14:27:58 +0300

    Vienots header

---
+++ assets/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP valoda - Masīvi</title>
+    <link rel="stylesheet" href="assets/style.css?v=0.1">
+    <script src="assets/script.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+    <header>
+        <i class="fa-solid fa-bars menu-toggle"></i>
+        <p>Nosaukums</p>
+        <a href="" class="btn">
+            Lietotājs
+            <i class="fa-solid fa-power-off"></i>
+        </a>
+    </header>
+
+    <aside>
+        <div class="aside-header">
+            <h1>PHP piemēros</h1>
+            <i class="fa-solid fa-xmark aside-close"></i>
+        </div>
+
+        <nav>
+            <a href="">Ievads PHP</a>
+            <a href="">PHP masīvi</a>
+            <a href="">PHP kontroles struktūras</a>
+            <a href="">PHP funkcijas</a>
+            <a href="">PHP pārbaudījums</a>
+            <a href="">PHP un MySQL</a>
+        </nav>
+    </aside>
+
+
+
+    
+</body>
+</html>
