8daa3cad534ff88e7846b96c9c4840cd4d795daf
LiteDuck1986 <emils2007smirnovs@gmail.com>
2026-04-21 08:46:31 +0300

    Funcijas -mājasdarbs

--- funkcijas.php
+++ funkcijas.php
@@ -48,27 +48,94 @@
             ?>
         </form>
         
+        <!-- 1. Piemērs: -->
         <h3>Funkcija, speciālā vietā apmaina vārdus:</h3>
         <?php
-            function apmainaVardu($masivs2){
-                $inputVards = $masivs;
-                $inputVieta = 
+            function apmainaVardu($vards, $vieta){
                 $text = "VARDS Gāja uz VIETA, patusēt ar saviem draugiem.";
-                $output = str_replace(["VARDS", "VIETA"], [$inputVards, $inputVieta], $text);
+                $output = str_replace(["VARDS", "VIETA"], [$vards, $vieta], $text);
                 return $output;
             }
         ?>
-        <P>Ievadi skaitļus savstarpēji tos atdalot ar komatu:</P>
+        <P><b>VARDS</b> Gāja uz <b>VIETA</b>, patusēt ar saviem draugiem.</P>
         <form method="POST">
-            <input type="text" name="vards">
-            <input type="text" name="vieta">
+            <input type="text" name="vards" placeholder="Vards">
+            <input type="text" name="vieta" placeholder="Vieta">
             <button type="submit" name="apmainit">Apmainīt</button>
             <?php
                 if(isset($_POST["apmainit"])){
-                    $input = $_POST["vards"];
-                    $masivs = explode(',', $skaitli);
-                    $rezultats = vid_aritm($masivs);
-                    echo "<p>Vidējais aritmētiskais ir <b>$rezultats<b></p>";
+                    $vards = $_POST["vards"];
+                    $vieta = $_POST["vieta"];
+
+                    if(empty($vards) || empty($vieta)){
+                        echo "<p style='color:red;'>Lūdzu ievadi gan vārdu, gan vietu!</p>";
+                    }
+                    else{
+                        $rezultats = apmainaVardu($vards, $vieta);
+                        echo "<p>$rezultats</p>";
+                    }
+                }
+            ?>
+        </form>
+
+        <!-- 2. Piemērs: -->
+        <h3>Funkcija, kas aprēķina taisnstūra laukumu:</h3>
+        <?php
+            function laukums($garums, $platums){
+                return $garums * $platums;
+            }
+        ?>
+        <P>Mērvienība ir kvadrātmetros (m²) un drīkst ievadīt tikai skaitļus!</P>
+        <form method="POST">
+            <input type="text" name="garums" placeholder="Garums">
+            <input type="text" name="platums" placeholder="Platums">
+            <button type="submit" name="aprekinatLaukumu">Aprēķināt</button>
+            <?php
+                if(isset($_POST["aprekinatLaukumu"])){
+                    $garums = $_POST["garums"];
+                    $platums = $_POST["platums"];
+
+                    if(empty($garums) || empty($platums)){ // pārbauda vai ir ievadītas abas vērtības
+                        echo "<p style='color:red;'>Ievadi abus skaitļus!</p>";
+                    }
+                    elseif(!is_numeric($garums) || !is_numeric($platums)){ // pārbauda vai ievadītās vērtības ir skaitļi
+                        echo "<p style='color:red;'>Drīkst ievadīt tikai skaitļus!</p>";
+                    }
+                    elseif($garums <= 0 || $platums <= 0){ // pārbauda vai skaitļi ir lielāki par 0
+                        echo "<p style='color:red;'>Skaitļiem jābūt lielākiem par 0!</p>";
+                    }
+                    else{
+                        $rezultats = laukums($garums, $platums);
+                        echo "<p>Taisnstūra laukums ir <b>$rezultats m²</b></p>";
+                    }
+                }
+            ?>
+        </form>
+
+        <!-- 3. Piemērs: -->
+        <h3>Funkcija, kas salīdzina divus skaitļus, pēc vērtības lieluma:</h3>
+        <?php
+            function lielakais($sk1, $sk2){
+                if($sk1 > $sk2){
+                    return $sk1;
+                }
+                else{
+                    return $sk2;
+                }
+
+            }
+        ?>
+        <P>Ievadi divus skaitļus:</P>
+        <form method="POST">
+            <input type="number" name="sk1" placeholder="Pirmais skaitlis" required>
+            <input type="number" name="sk2" placeholder="Otrais skaitlis" required>
+            <button type="submit" name="salidzinat">Salīdzināt</button>
+            <?php
+                if(isset($_POST["salidzinat"])){
+                    $sk1 = $_POST["sk1"];
+                    $sk2 = $_POST["sk2"];
+                    $rezultats = lielakais($sk1, $sk2);
+                    echo "<p>Lielākais skaitlis ir <b>$rezultats</b></p>";
                 }
             ?>
         </form>
